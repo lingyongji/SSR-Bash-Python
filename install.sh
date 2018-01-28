761d5c7fd36a34b4353a734baec85ee724e356a0
lingyongji <32590859+lingyongji@users.noreply.github.com>
2018-01-28 23:07:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-. /usr/local/SSR-Bash-Python/config.sh
 
 #Check Root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
@@ -60,8 +59,8 @@ fi
 
 #Install SSR and SSR-Bash
 cd /usr/local
-git clone https://github.com/shadowsocksr/shadowsocksr.git
-git clone https://github.com/${GH_REPO}.git
+git clone https://github.com/lingyongji/shadowsocksr.git
+git clone https://github.com/lingyongji/SSR-Bash-Python.git
 cd /usr/local/shadowsocksr
 bash initcfg.sh
 
