fc38838c6a5d0c98ecb9b7937dc3de3c9944912c
天意jily <32590859+lingyongji@users.noreply.github.com>
2018-01-31 01:06:05 +0800

    Update show_flow.py

--- show_flow.py
+++ show_flow.py
@@ -10,7 +10,7 @@ print "用户名\t端口\t已用流量\t流量限制"
 for x in json:
   #Convert Unit To MB
   transfer_enable_int = int(x[u"transfer_enable"])/1024/1024;
-  d_int = round(int(x[u"d"])/1024/1024,2);
+  d_int = round(float(int(x[u"d"])/1024/1024),2);
   transfer_unit = "MB"
   d_unit = "MB"
 
@@ -19,7 +19,7 @@ for x in json:
   	transfer_enable_int = transfer_enable_int/1024;
   	transfer_unit = "GB"
   if(d_int > 1024):
-  	d_int = round(d_int/1024,2);
+  	d_int = round(float(d_int/1024),2);
   	d_unit = "GB"
 
   #Print In Format
