2935e967d6aa64880f51ffa026750699a39e8cbe
lingyongji <lingyongji@hengtiansoft.com>
2018-01-30 21:47:20 +0800

    UI

--- www/cgi-bin/show_info.py
+++ www/cgi-bin/show_info.py
@@ -27,15 +27,29 @@ for x in json:
 		portexist=1
 		if(str(x[u"passwd"]) == str(getpasswd)):
 			passwdcorrect=1
+			transfer_enable_int = int(x[u"transfer_enable"])/1024/1024;
+			d_int = int(x[u"d"])/1024/1024;
+			transfer_unit = "MB"
+			d_unit = "MB"
 			jsonmethod=str(x[u"method"])
 			jsonobfs=str(x[u"obfs"])
 			jsonprotocol=str(x[u"protocol"])
+			#流量单位转换
+			if(transfer_enable_int > 1024):
+				transfer_enable_int = transfer_enable_int/1024
+				transfer_unit = "GB"
+			if(transfer_enable_int > 1024):
+				d_int = d_int/1024
+				d_unit = "GB"
 		break
 
 if(portexist==0):
 	getport = "未找到此端口，请检查是否输入错误！"
 	myip = ""
-	getpasswd = ""
+	transfer_enable_int = ""
+	d_int = ""
+	transfer_unit = ""
+	d_unit = ""
 	jsonmethod = ""
 	jsonprotocol = ""
 	jsonobfs = ""
@@ -43,106 +57,79 @@ if(portexist==0):
 if(portexist!=0 and passwdcorrect==0):
 	getport = "连接密码输入错误，请重试"
 	myip = ""
-	getpasswd = ""
+	transfer_enable_int = ""
+	d_int = ""
+	transfer_unit = ""
+	d_unit = ""
 	jsonmethod = ""
 	jsonprotocol = ""
 	jsonobfs = ""
 
 
-header = '''
+#打印返回的内容
+html='''
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta content="IE=edge" http-equiv="X-UA-Compatible">
-	<meta content="initial-scale=1.0, width=device-width" name="viewport">
-	<title>连接信息</title>
-	<!-- css -->
-	<link href="../css/base.min.css" rel="stylesheet">
-
-	<!-- favicon -->
-	<!-- ... -->
-
-	<!-- ie -->
-    <!--[if lt IE 9]>
-        <script src="../js/html5shiv.js" type="text/javascript"></script>
-        <script src="../js/respond.js" type="text/javascript"></script>
-    <![endif]-->
-    
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="initial-scale=1.0, width=device-width" name="viewport">
+    <title>端口信息</title>
+    <link href="../css/base.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="content">
         <div class="content-heading">
             <div class="container">
-                <h1 class="heading">&nbsp;&nbsp;连接信息</h1>
+                <h1 class="heading">&nbsp;&nbsp;端口信息</h1>
             </div>
         </div>
         <div class="content-inner">
             <div class="container">
-'''
-
-
-footer = '''
-</div>
+                <div class="card-wrap">
+                    <div class="row">
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="card card-green">
+                                <a class="card-side" href="/"><span class="card-heading">端口信息</span></a>
+                                <div class="card-main">
+                                    <div class="card-inner">
+                                        <p>
+                                            <strong>服务器地址：</strong> %s </br></br>
+                                            <strong>连接端口：</strong> %s </br></br>
+                                            <strong>流量信息：</strong> %s %s / %s %s</br></br>
+                                            <strong>加密方式： </strong>%s </br></br>
+                                            <strong>协议方式： </strong></br>%s </br></br>
+                                            <strong>混淆方式：</strong> </br>%s
+                                        </p>
+                                    </div>
+                                    <div class="card-action">
+                                        <ul class="nav nav-list pull-left">
+                                            <li>
+                                                <a href="../index.html"><span class="icon icon-check"></span>&nbsp;返回</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-	<footer class="footer">
-		<div class="container">
-			<p>Function Club</p>
-		</div>
-	</footer>
-
-	<script src="../js/base.min.js" type="text/javascript"></script>
+    <footer class="footer">
+        <div class="container">
+            <p>Function Club</p>
+        </div>
+    </footer>
+    <script src="../js/base.min.js" type="text/javascript"></script>
 </body>
 </html>
-'''
-
-
-#打印返回的内容
-print header
-
-formhtml = '''
-
-<div class="card-wrap">
-					<div class="row">
-						
-						
-						<div class="col-lg-4 col-sm-6">
-							<div class="card card-green">
-								<a class="card-side" href="/"><span class="card-heading">连接信息</span></a>
-								<div class="card-main">
-									<div class="card-inner">
-									<p>
-										<strong>服务器地址：</strong> %s </br></br>
-										<strong>连接端口：</strong> %s </br></br>
-										<strong>连接密码：</strong> %s </br></br>
-										<strong>加密方式： </strong> %s </br></br>
-										<strong>协议方式： </strong> </br>%s </br></br>
-										<strong>混淆方式：</strong> </br>%s 
-										</p>
-									</div>
-									<div class="card-action">
-										<ul class="nav nav-list pull-left">
-											<li>
-												<a href="../index.html"><span class="icon icon-check"></span>&nbsp;返回</a>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-						
-						
-						
-					</div>
-				</div>
-
-
-
 
 '''
 
-print formhtml % (myip,getport,getpasswd,jsonmethod,jsonprotocol,jsonobfs)
-print footer
+print html % (myip,getport,d_int,d_unit,transfer_enable_int,transfer_unit,jsonmethod,jsonprotocol,jsonobfs)
+
 f.close();
 
