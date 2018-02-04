ad77f45393e5a94f27bc4ac121173022b6ade989
天意jily <32590859+lingyongji@users.noreply.github.com>
2018-02-04 21:59:26 +0800

    Update show_all_user_info.py

--- user/show_all_user_info.py
+++ user/show_all_user_info.py
@@ -8,5 +8,5 @@ json = json.load(f);
 print "用户名\t端口\t加密\t\t密码"
 
 for x in json:
-  print "%s\t%s\t%s\t%s" %(x[u"user"],x[u"port"],x[u"method"],x[u"passwd"])
+  print "%-8s\t%-8s\t%-16s\t%-16s" %(x[u"user"],x[u"port"],x[u"method"],x[u"passwd"])
 f.close();
