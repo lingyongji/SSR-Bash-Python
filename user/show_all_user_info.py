136494c37eb8aa1acedb49569118a34570ecace1
天意jily <32590859+lingyongji@users.noreply.github.com>
2018-02-20 22:46:57 +0800

    Update show_all_user_info.py

--- user/show_all_user_info.py
+++ user/show_all_user_info.py
@@ -5,8 +5,8 @@ f = file("/usr/local/shadowsocksr/mudb.json");
 
 json = json.load(f);
 
-print "用户名\t\t端口\t加密\t\t密码\t\t协议"
+print "用户名\t端口\t加密\t\t密码\t\t协议"
 
 for x in json:
-  print "%-8s\t%-4s\t%-8s\t%-8s\t%-8s" %(x[u"user"],x[u"port"],x[u"method"],x[u"passwd"],x[u"protocol"])
+  print "%-4s\t%-4s\t%-8s\t%-8s\t%-8s" %(x[u"user"],x[u"port"],x[u"method"],x[u"passwd"],x[u"protocol"])
 f.close();
